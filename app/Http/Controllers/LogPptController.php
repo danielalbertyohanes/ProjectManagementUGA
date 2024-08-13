9f0bc0c70488a36fe0f01c1029be9c94b014f579
FelixLeo99 <felixleonardy19@gmail.com>
2024-08-13 21:14:37 +0700

    log controller

--- app/Http/Controllers/LogPptController.php
+++ app/Http/Controllers/LogPptController.php
@@ -2,9 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LogPpt;
+
 use Illuminate\Http\Request;
 
 class LogPptController extends Controller
 {
-    //
+    public function index()
+    {
+        $logPpts = LogPpt::logPpts();
+        return view('logPpts.index', compact('logPpts'));
+    }
+
+    //get log (status, desc)
+    public function getStatusAndDesc($user_id, $ppt_id)
+    {
+        return LogPpt::getStatusAndDesc($user_id, $ppt_id);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'status' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        $logPpt = LogPpt::insertLog($data);
+
+        return redirect()->route('logPpts.index')->with('status', 'Berhasil Tambah');
+    }
+
+    //update
+    public function update($user_id, $ppt_id, Request $request)
+    {
+        $data = $request->validate([
+            'status' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        $logPpt = LogPpt::updateLog($user_id, $ppt_id, $data);
+
+        return redirect()->route('logPpts.index')->with('status', 'Berhasil Update');
+    }
 }
--- app/Http/Controllers/LogVideosController.php
+++ app/Http/Controllers/LogVideosController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LogVideo;
+
 use Illuminate\Http\Request;
 
 class LogVideosController extends Controller
 {
-    //
+    public function index()
+    {
+        $logVideos = LogVideo::logVideos();
+        return view('logVideos.index', compact('logVideos'));
+    }
+    //get log (status, desc)
+    public function getStatusAndDesc($user_id, $video_id)
+    {
+        return LogVideo::getStatusAndDesc($user_id, $video_id);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'status' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        $logVideo = LogVideo::insertLog($data);
+
+        return redirect()->route('logVideos.index')->with('status', 'Berhasil Tambah');
+    }
+    //update
+    public function update($user_id, $video_id, Request $request)
+    {
+        $data = $request->validate([
+            'status' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        $logVideo = LogVideo::updateLog($user_id, $video_id, $data);
+
+        return redirect()->route('logVideos.index')->with('status', 'Berhasil Update');
+    }
 }
